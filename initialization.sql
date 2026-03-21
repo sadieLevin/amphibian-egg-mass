4f45b2569ea7a632031038ba5380a080a8be9420
Sadie Levin <sadiew.levin@gmail.com>
2026-03-21 15:22:32 -0700

    heyy we got the data in the thing :3

--- initialization.sql
+++ initialization.sql
@@ -1,6 +1,6 @@
 -- CREATE DATABASE amphibian_mass;
 
-CREATE TABLE staging_masses
+CREATE TABLE staging_masses(
 SurveyID VARCHAR(63),
 DateTime VARCHAR(63),
 Latitude VARCHAR(63),
@@ -9,15 +9,22 @@ Accuracy_m VARCHAR(63),
 NumberOfEggMasses VARCHAR(63),
 SpeciesCode VARCHAR(63),
 EggMassSubstrate VARCHAR(63),
-Comments VARCHAR(63)
+Comments VARCHAR(255)
+);
 
-CREATE TABLE staging_weather
+CREATE TABLE staging_weather(
 time VARCHAR(63),
-temperature_2m_mean_(°C) VARCHAR(63),
-sunshine_duration_(s) VARCHAR(63),
-precipitation_sum_(mm) VARCHAR(63),
-rain_sum_(mm) VARCHAR(63),
-snowfall_sum_(cm) VARCHAR(63),
-wind_speed_10m_max_(km/h) VARCHAR(63)
+temperature_2m_mean_C VARCHAR(63),
+sunshine_duration_s VARCHAR(63),
+precipitation_sum_mm VARCHAR(63),
+rain_sum_mm VARCHAR(63),
+snowfall_sum_cm VARCHAR(63),
+wind_speed_10m_max_kmh VARCHAR(63)
+);
 
-
+\copy staging_masses(
+    SurveyID,DateTime,Latitude,Longitude,Accuracy_m,NumberOfEggMasses,SpeciesCode,EggMassSubstrate,Comments
+)
+FROM './data/EggMasses.csv'
+DELIMITER ','
+CSV HEADER;
